19622e7bb05348c96a69fadca4c99882b4aab3fa
Alex Huebner <alexhbnr@gmail.com>
2023-08-02 16:14:50 +0200

    Delete redundant wrappers

--- workflow/wrappers/subset_bam_to_mag/wrapper.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Alexander Hübner"
-__license__ = "MIT"
-
-from snakemake.shell import shell
-
-shell(
-    "(cat {snakemake.input[0]} | xargs samtools view -hu {snakemake.params.bam} > {snakemake.output[0]})"
-)
